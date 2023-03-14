e645b7079267e461cba638807bc1b20718e902f3
dolgogae <ohsh0731@gmail.com>
2023-03-14 21:12:17 +0900

    swagger 추가

--- src/main/java/com/example/templateproject/example/controller/TestLogController.java
+++ src/main/java/com/example/templateproject/example/controller/TestLogController.java
@@ -1,16 +1,19 @@
 package com.example.templateproject.example.controller;
 
 import com.example.templateproject.example.service.TestLogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/test/log")
-@RequiredArgsConstructor
-@Slf4j
+@RequiredArgsConstructor @Slf4j
 public class TestLogController {
 
     private final TestLogService testLogService;
@@ -20,8 +23,16 @@ public class TestLogController {
         testLogService.logging();
     }
 
+    @Operation(summary = "slack 로그 전송", description = "slack에 로그가 전송됩니다.", tags = {"Test Controller"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping("slack")
-    public void slackLog(){
+    public String slackLog(){
         testLogService.logging();
+        return "Success sent to slack";
     }
 }
